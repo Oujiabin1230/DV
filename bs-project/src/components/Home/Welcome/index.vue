e6975b6725323f288db398a66145239789005142
dogerddddfd <1739988739@qq.com>
2022-12-29 10:45:40 +0800

    修改网页标题，welecome背景占位

--- bs-project/src/components/Home/Welcome/index.vue
+++ bs-project/src/components/Home/Welcome/index.vue
@@ -1,5 +1,5 @@
 <template>
-   <div>
+   <div class="welcome-warp">
       <h2>Welcome</h2>
    </div>
 </template>
@@ -8,5 +8,11 @@
 export default {};
 </script>
  
- <style>
+ <style lang="less" scoped>
+.welcome-warp {
+   width: 100%;
+   height: 100%;
+   // 改这一行就行
+   background-image: url('https://img2.baidu.com/it/u=1691031580,1368026143&fm=253&fmt=auto&app=138&f=PNG?w=1045&h=500');
+}
 </style>
